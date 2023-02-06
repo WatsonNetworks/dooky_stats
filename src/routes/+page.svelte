aadf0e9dfc9cb897bdc80000786db647f7f70346
Justin B. Watson <justin@brimelive.com>
2023-02-06 09:59:03 -0600

    fix mobile link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -237,7 +237,7 @@ Looking for a stream overlay with your stats? <a class="link" href="https://twit
     <span class="pink">&nbsp; TIER 4:</span> {tierStats[4]}
 <br><br>
     <div style="width: 400px; display: flex; flex-direction: column; max-width: calc(100vw - 18px);">
-    <a class="linkButton" on:click={linkClick}>
+    <a class="linkButton" href="https://twitter.com/dookey_boost" target="_blank">
       <img src="/final.png" style="width: 100%;"/>
       </a>
       <span style="text-align: right; padding-top: 2px;">#AD</span>
